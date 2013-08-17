d0da6c18a00c2b0d34b0c2e4eadb747829f26e90
Kan Fushihara <kan.fushihara@gmail.com>
2013-08-17 12:27:35 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::TVSonet - It's new $module
+
+# SYNOPSIS
+
+    use WebService::TVSonet;
+
+# DESCRIPTION
+
+WebService::TVSonet is ...
+
+# LICENSE
+
+Copyright (C) Kan Fushihara.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kan Fushihara <kan.fushihara@gmail.com>
---
+++ lib/WebService/TVSonet.pm
@@ -0,0 +1,39 @@
+package WebService::TVSonet;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::TVSonet - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::TVSonet;
+
+=head1 DESCRIPTION
+
+WebService::TVSonet is ...
+
+=head1 LICENSE
+
+Copyright (C) Kan Fushihara.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kan Fushihara E<lt>kan.fushihara@gmail.comE<gt>
+
+=cut
+
